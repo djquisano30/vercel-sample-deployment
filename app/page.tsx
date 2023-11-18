ff5b3ba1b644c175c0bbeef03b7fac8e6bc9d4b0
djquisano30 <daryljettquisano@gmail.com>
2023-11-18 14:22:52 +0800

    ClickCountChanges

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+"use client";
+import { Button } from "@nextui-org/button";
+import { title } from "@/components/primitives";
+import { useState } from "react";
 
 export default function Home() {
+  const [clickCount, setClickCount] = useState(0);
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>This is a&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>Testing&nbsp;</h1>
+        <h1 className={title()}>The button has been clicked&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>{clickCount}&nbsp;</h1>
         <br />
-        <h1 className={title()}>website for MIT 241.</h1>
+        <h1 className={title()}>times</h1>
       </div>
+      <Button
+        size="lg"
+        color="primary"
+        onClick={() => {
+          setClickCount(clickCount + 1);
+        }}
+      >
+        Click Me
+      </Button>
     </section>
   );
 }
